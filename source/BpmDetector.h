f7959fe8dd1c6a33c39d6eb8876efd6b6ee35736
abk <andrewking1597@gmail.com>
2022-01-13 13:31:31 -0500

    BpmDetector documentation

--- source/BpmDetector.h
+++ source/BpmDetector.h
@@ -29,23 +29,84 @@ public:
      * @param blocksPerWindow the window size to use (in blocks)
      */
     BpmDetector(int samplesPerBlock=1024, int blocksPerWindow=40);
+
+    /**
+     * Detects the BPM of some audio data. Must first set the audio data by calling setAudio().
+     * @see setAudio()
+     * @param minBpm the minimum acceptable BPM value
+     * @param maxBpm the maximum acceptable BPM value
+     * @returns the detected BPM
+     */
     float detectBpm(int minBpm=90, int maxBpm=180);
     //================================================================
     // Getters & Setters
+    /**
+     * Sets the audio whose BPM will be measured. This MUST be called before calling detectBpm()
+     * @see detectBpm()
+     * @param audio reference to the audio data
+     */
     void setAudio(std::vector<std::vector<float> > &audio);
+
+    /** @returns the number of samples per block */
     int getSamplesPerBlock();
+
+    /** @returns the number of blocks per window */
     int getBlocksPerWindow();
+
+    /**
+     * Sets the number of samples that make up a block
+     * @param samplesPerBlock
+     */
     void setSamplesPerBlock(int samplesPerBlock);
+
+    /**
+     * Sets the number of blocks that make up a window
+     * @param blocksPerWindow
+     */
     void setBlocksPerWindow(int blocksPerWindow);
+
 private:
+    /**
+     * Computes the sound energy threshold for the given window.
+     *
+     * Computes the sound energy threshold for the given window. The sound energy threshold is the
+     * amount of sound energy a block in the given window needs to be considered to contain a beat.
+     * @param w specs of the window being analyzed
+     * @return the sound energy threshold
+     */
     float computeThreshold(WindowSpecs w);
+
+    /**
+     * Computes the average sound energy produced by the blocks in the given window
+     * @param w specs of the window being analyzed
+     * @return the average energy
+     * @see computeBlockEnergy()
+     */
     float computeAvgBlockEnergy(WindowSpecs w);
+
+    /**
+     * Computes the sound energy produced by the given block
+     * @param blockNum the index of the block being analyzed
+     * @return the sound energy produced by the given block
+     */
     float computeBlockEnergy(int blockNum);
+
+    /**
+     * vector of vectors of floats, holds audio data that was set using setAudio().
+     * The inner vectors each represent a channel.
+     * Example: 2 channels and 10 samples might look like...
+     * [0.01, 0.23, -0.43, 0.2, -0.68, 0.2], [0.01, 0.19, -0.45, 0.23, -0.7, 0.22]]
+     * @see setAudio()
+     */
     std::vector<std::vector<float> > m_data;
-    int m_samplesPerBlock;
-    int m_blocksPerWindow;
+    int m_samplesPerBlock; /**< number of samples in each block */
+    int m_blocksPerWindow; /**< number of blocks in each window */
+    int m_numSamples; /**< number of samples in each channel of m_data */
+    /**
+     * number of samples in each window. This is easily calculated as m_samplesPerBlock * m_blocksPerWindow but
+     * it's here for convenience.
+     */
     int m_samplesPerWindow;
-    int m_numSamples;
 };
 
 
