305b32dd286c1912ef6799521055b06000a79bb0
abk <andrewking1597@gmail.com>
2022-01-13 11:22:07 -0500

    BpmDetector constructor

--- source/BpmDetector.cpp
+++ source/BpmDetector.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "BpmDetector.h"
+
+BpmDetector::BpmDetector(int samplesPerBlock, int blocksPerWindow)
+{
+    m_samplesPerBlock = samplesPerBlock;
+    m_blocksPerWindow = blocksPerWindow;
+    m_samplesPerWindow = m_blocksPerWindow * m_samplesPerBlock;
+    m_numSamples = 0;
+}
--- source/BpmDetector.h
+++ source/BpmDetector.h
@@ -5,9 +5,29 @@
 #ifndef BPMDETECTOR_BPMDETECTOR_H
 #define BPMDETECTOR_BPMDETECTOR_H
 
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+typedef struct {
+    int id;
+    int startSample;
+    int endSample;
+    int startBlock;
+    int endBlock;
+    int avgEnergy;
+} WindowSpecs;
 
 class BpmDetector {
-
+public:
+    BpmDetector(int samplesPerBlock=1024, int blocksPerWindow=40);
+private:
+    std::vector<std::vector<float> > m_data;
+    int m_samplesPerBlock;
+    int m_blocksPerWindow;
+    int m_samplesPerWindow;
+    int m_numSamples;
 };
 
 
