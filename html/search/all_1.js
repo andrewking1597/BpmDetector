5f168eaa46b45eee491516ea500e55b33c211d32
abk <andrewking1597@gmail.com>
2022-01-18 16:36:55 -0500

    set readme as doxy mainpage

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['bpmdetector_0',['BpmDetector',['../class_bpm_detector.html',1,'BpmDetector'],['../class_bpm_detector.html#a912f475dcd2f8f8da3dfb560a935f214',1,'BpmDetector::BpmDetector()'],['../md__r_e_a_d_m_e.html',1,'(Global Namespace)']]],
+  ['bpmdetector_0',['BpmDetector',['../class_bpm_detector.html',1,'BpmDetector'],['../class_bpm_detector.html#a912f475dcd2f8f8da3dfb560a935f214',1,'BpmDetector::BpmDetector()'],['../index.html',1,'(Global Namespace)']]],
   ['bpmdetector_2eh_1',['BpmDetector.h',['../_bpm_detector_8h.html',1,'']]]
 ];
